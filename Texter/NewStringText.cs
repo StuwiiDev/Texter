50694bd63d6cfe10bd293e85645afb25adf9b78d
Stuart Maskell <stuart.maskell@york.gov.uk>
2017-04-27 09:19:02 +0100

    Add project files.

---
+++ Texter/NewStringText.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Texter
+{
+
+        public class NewStringText
+        {
+            public int NumberOfLines;
+            public string FirstLine;
+
+            public string ToTxt(string fileName, string targetFile)
+            {
+                StreamWriter writeToFile = new StreamWriter(targetFile);
+                var lines = File.ReadAllLines(fileName);
+                var csv = lines.ToArray();
+
+                foreach (var c in csv)
+                {
+                    var replace = c.Replace("\t", "");
+                    writeToFile.WriteLine(replace);
+                }
+                writeToFile.Close();
+
+                var test = File.ReadLines(targetFile).First();
+                FirstLine = File.ReadLines(targetFile).First();
+                NumberOfLines = File.ReadLines(targetFile).Count();
+
+                return test;
+            }
+
+            
+        }
+}
---
+++ Texter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Texter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nThis program removes tabbed spaces from text files and outputs to a new specified file\n");
+
+            if (args.Length == 2)
+            {
+
+                Console.WriteLine("Input TXT: {0}", args[0]);
+                Console.WriteLine("Output TXT: {0}", args[1]);
+                Console.WriteLine("Program to remove tabbed spaces in txt files");
+                var newFile = new NewStringText();
+                newFile.ToTxt(args[0], args[1]);
+                Console.WriteLine("First line: " + newFile.FirstLine);
+                Console.WriteLine("Number of Lines: " + newFile.NumberOfLines);
+
+
+
+            }
+            else
+            {
+
+                Console.WriteLine("Correct way of entering. texter {input txt name inc .txt} {output txt name inc .txt}");
+            }
+        }
+    }
+}
